9adf77b8b7a6dafe35eb4abb12be1dd4360a8741
b-sirius <padfoot_07@163.com>
2017-10-31 20:18:21 +0800

    listener

--- src/common.js
+++ src/common.js
@@ -1,5 +1,24 @@
-'use strict';
+'use strict'
 
+let get = (selector) => {
+    return document.querySelector(selector)
+}
+
+let getAbsoluteTop = (el) => {　　　　
+    let actualTop = el.offsetTop　　　　
+    let current = el.offsetParent　　　　
+    while (current !== null) {　　　　　　
+        actualTop += current.offsetTop　　　　　　
+        current = current.offsetParent　　　　
+    }　　　　
+    return actualTop
+}
+
+let getRelativeWindowTop = (el) => {
+    return el.getBoundingClientRect().top
+}
 
 export {
+    get,
+    getRelativeWindowTop
 }
--- src/script.js
+++ src/script.js
@@ -1,3 +1,31 @@
-'use strict';
-import './reset.scss';
-import './style.scss';
+'use strict'
+import './reset.scss'
+import './style.scss'
+
+import {
+    get,
+    getRelativeWindowTop
+} from './common.js'
+
+let sidebarListener = {
+    el: get('#js-sidebar'),
+    rules: [{
+        className: 'active',
+        isActive() {
+            let compareEl = get('#js-video');
+            if (getRelativeWindowTop(compareEl) * -1 > compareEl.offsetHeight / 3)
+                return true
+            return false
+        },
+        isDormant() {}
+    }]
+}
+
+let {el, rules} = sidebarListener;
+window.addEventListener('scroll', () => {
+    for (let rule of rules) {
+        if (rule.isActive()) {
+            el.classList.add(rule.className);
+        }
+    }
+});
