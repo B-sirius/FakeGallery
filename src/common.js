af136fd67c706822c1c426aaf8b23155cddf9d61
b-sirius <padfoot_07@163.com>
2017-11-04 17:17:17 +0800

    scroll change bg pos

--- src/common.js
+++ src/common.js
@@ -1,24 +1,39 @@
-'use strict'
+'use strict';
 
 let get = (selector) => {
     return document.querySelector(selector)
 }
 
 let getAbsoluteTop = (el) => {　　　　
-    let actualTop = el.offsetTop　　　　
-    let current = el.offsetParent　　　　
+    let actualTop = el.offsetTop;　　　　
+    let current = el.offsetParent;　　　　
     while (current !== null) {　　　　　　
-        actualTop += current.offsetTop　　　　　　
-        current = current.offsetParent　　　　
+        actualTop += current.offsetTop;　　　　　　
+        current = current.offsetParent;　　　　
     }　　　　
-    return actualTop
+    return actualTop;
 }
 
 let getRelativeWindowTop = (el) => {
-    return el.getBoundingClientRect().top
+    return el.getBoundingClientRect().top;
 }
 
-export {
+let throttle = (fn ,interval = 50) => {
+    let timeId = null;
+    return () => {
+        if (timeId === null) {
+            timeId = setTimeout(() => {
+                fn();
+
+                clearTimeout(timeId);
+                timeId = null;
+            }, interval);
+        }
+    }
+}
+
+export default {
     get,
-    getRelativeWindowTop
+    getRelativeWindowTop,
+    throttle
 }
--- src/script.js
+++ src/script.js
@@ -1,31 +1,112 @@
-'use strict'
-import './reset.scss'
-import './style.scss'
+'use strict';
+import './reset.scss';
+import './style.scss';
 
-import {
-    get,
-    getRelativeWindowTop
-} from './common.js'
+import _ from './common.js';
 
-let sidebarListener = {
-    el: get('#js-sidebar'),
+let sidebarClass = {
+    // 监听器作用对象
+    el: _.get('#js-sidebar'),
+    // 与该对象有关的规则
+    rules: [{
+        // 有关的class
+        className: 'active',
+        // class属性是否触发
+        isActive() {
+            let compareEl;
+            if (this._cache['#js-video']) {
+                compareEl = this._cache['#js-video'];
+            } else {
+                compareEl = _.get('#js-video');
+                this._cache['#js-video'] = compareEl;
+            }
+
+            if (_.getRelativeWindowTop(compareEl) * -1 > compareEl.offsetHeight / 3)
+                return true;
+            return false;
+        },
+        // class属性是否被隐藏
+        isDormant() {
+            let compareEl;
+            if (this._cache['#js-video']) {
+                compareEl = this._cache['#js-video'];
+            } else {
+                compareEl = _.get('#js-video');
+                this._cache['#js-video'] = compareEl;
+            }
+
+            if (_.getRelativeWindowTop(compareEl) * -1 > compareEl.offsetHeight / 3)
+                return false;
+            return true;
+        },
+        // 私有缓存
+        _cache: {},
+    }]
+};
+
+let bgShapeClass = {
+    el: _.get('#js-bgShape'),
     rules: [{
         className: 'active',
         isActive() {
-            let compareEl = get('#js-video');
-            if (getRelativeWindowTop(compareEl) * -1 > compareEl.offsetHeight / 3)
-                return true
-            return false
-        },
-        isDormant() {}
-    }]
-}
+            let compareEl;
+            if (this._cache['#js-video']) {
+                compareEl = this._cache['#js-video'];
+            } else {
+                compareEl = _.get('#js-video');
+                this._cache['#js-video'] = compareEl;
+            }
 
-let {el, rules} = sidebarListener;
-window.addEventListener('scroll', () => {
-    for (let rule of rules) {
-        if (rule.isActive()) {
-            el.classList.add(rule.className);
+            if (_.getRelativeWindowTop(compareEl) < 0)
+                return true;
+            return false;
+        },
+        isDormant() {
+            let compareEl;
+            if (this._cache['#js-video']) {
+                compareEl = this._cache['#js-video'];
+            } else {
+                compareEl = _.get('#js-video');
+                this._cache['#js-video'] = compareEl;
+            }
+
+            if (_.getRelativeWindowTop(compareEl) < 0)
+                return false;
+            return true;
+        },
+        _cache: {},
+    }],
+};
+
+let classListenerList = [];
+classListenerList.push(sidebarClass);
+classListenerList.push(bgShapeClass);
+
+let posFn = (() => {
+    let el = _.get('#js-bgShapeWrapper');
+    return function() {
+        let f = 0.55 + window.scrollY * 0.002;
+        el.style.transform = `matrix3d(1, -0.36397, 0, 0, 0, ${f}, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)`;
+    }
+})();
+
+(() => {
+    let classToggle = () => {
+        for (let listener of classListenerList) {
+            let {el, rules} = listener;
+            for (let rule of rules) {
+                if (!el.classList.contains(rule.className) && rule.isActive()) {
+                    el.classList.add(rule.className);
+                } else if (el.classList.contains(rule.className) && rule.isDormant()) {
+                    el.classList.remove(rule.className);
+                }
+            }
         }
     }
-});
+
+    let scrollHandler = _.throttle(() => {
+        classToggle();
+        posFn();
+    }, 30);
+    window.addEventListener('scroll', scrollHandler);
+})();
